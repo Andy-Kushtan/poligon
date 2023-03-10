c539f89b6c13a48e350d104131df47a7e0339ffa
Andy-Kushtan <kushtanandriy17@gmail.com>
2023-03-10 22:34:21 +0200

    add 10 win numbers in arr

--- script.js
+++ script.js
@@ -29,7 +29,85 @@ const btnMAX = document.querySelector(".btnmax");
 const balance = document.querySelector(
   ".main-roullet__balance__balance__amount"
 );
+const btnAllRed = document.querySelector(".display-interface__all-bet__coulor");
+const btnRED1 = document.querySelector(
+  ".display-interface__all-bets__numbers-red-1"
+);
+const btnRED2 = document.querySelector(
+  ".display-interface__all-bets__numbers-red-2"
+);
+const btnRED3 = document.querySelector(
+  ".display-interface__all-bets__numbers-red-3"
+);
+const btnRED4 = document.querySelector(
+  ".display-interface__all-bets__numbers-red-4"
+);
+const btnRED5 = document.querySelector(
+  ".display-interface__all-bets__numbers-red-5"
+);
+const btnRED6 = document.querySelector(
+  ".display-interface__all-bets__numbers-red-6"
+);
+const btnRED7 = document.querySelector(
+  ".display-interface__all-bets__numbers-red-7"
+);
+const bntZero = document.querySelector(".display-interface__zero-box__zero");
+const bntAllBlack = document.querySelector(
+  ".display-interface__all-bet__coulor-black"
+);
+const btnBlack8 = document.querySelector(
+  ".display-interface__all-bets__numbers-black-8"
+);
+const btnBlack9 = document.querySelector(
+  ".display-interface__all-bets__numbers-black-9"
+);
+const btnBlack10 = document.querySelector(
+  ".display-interface__all-bets__numbers-black-10"
+);
+const btnBlack11 = document.querySelector(
+  ".display-interface__all-bets__numbers-black-11"
+);
+const btnBlack12 = document.querySelector(
+  ".display-interface__all-bets__numbers-black-12"
+);
+const btnBlack13 = document.querySelector(
+  ".display-interface__all-bets__numbers-black-13"
+);
+const btnBlack14 = document.querySelector(
+  ".display-interface__all-bets__numbers-black-14"
+);
+const counterRed = document.querySelector(".counter__red");
+const counterZero = document.querySelector(".counter__zero");
+const counterBlack = document.querySelector(".counter__black");
+const totalBetAmountRed = document.querySelector(
+  ".display-players__totalbet__amount-red"
+);
+const totalBetAmountZero = document.querySelector(
+  ".display-players__totalbet__amount-zero"
+);
+const totalBetAmountBlack = document.querySelector(
+  ".display-players__totalbet__amount-black"
+);
+const totalPlayersRed = document.querySelector(
+  ".display-players__players__number-red"
+);
+const totalPlayersZero = document.querySelector(
+  ".display-players__players__number-zero"
+);
+const totalPlayersBlack = document.querySelector(
+  ".display-players__players__number-black"
+);
+const RedActivePlayers = document.querySelector(
+  ".active-players__container-red"
+);
+const RedAPBetAmount = document.querySelector(
+  ".active-players__betamount__amount-red"
+);
+const winNumbersArr = document.querySelector(
+  ".main-roullet__win-numbers__numbers"
+);
 
+//////////////////chat rules poap
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove("hidden");
@@ -53,9 +131,257 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+///////////////////////bets buttons for increasing and decreasing bet
+balanceReload.addEventListener("click", function () {
+  balanceReload.classList.toggle("spin");
+});
+
+btnClear.addEventListener("click", function () {
+  inputBetAmount.value = 0;
+});
+
+btn10.addEventListener("click", function () {
+  inputBetAmount.value = +inputBetAmount.value + 10;
+});
+
+btn100.addEventListener("click", function () {
+  inputBetAmount.value = +inputBetAmount.value + 100;
+});
+
+btn1000.addEventListener("click", function () {
+  inputBetAmount.value = +inputBetAmount.value + 1000;
+});
+
+btn12.addEventListener("click", function () {
+  inputBetAmount.value = +inputBetAmount.value / 2;
+});
+
+btnX2.addEventListener("click", function () {
+  inputBetAmount.value = +inputBetAmount.value * 2;
+});
+
+btnMAX.addEventListener("click", function () {
+  inputBetAmount.value = balance.textContent;
+});
+
+let clickCounterActivePlayers = 0;
+btnAllRed.addEventListener("click", function () {
+  if (
+    inputBetAmount.value > 0 &&
+    +balance.textContent >= inputBetAmount.value
+  ) {
+    counterRed.textContent = +counterRed.textContent + +inputBetAmount.value;
+    totalBetAmountRed.textContent =
+      +totalBetAmountRed.textContent + +inputBetAmount.value;
+
+    // RedAPBetAmount.textContent = counterRed.textContent;
+  }
+
+  if (
+    inputBetAmount.value > 0 &&
+    +balance.textContent >= inputBetAmount.value &&
+    clickCounterActivePlayers === 0
+  ) {
+    clickCounterActivePlayers++;
+    totalPlayersRed.textContent = +totalPlayersRed.textContent + 1;
+    RedActivePlayers.innerHTML = "";
+    const html = `
+                    <div class="active-players">
+                      <img class="active-players__avatar" src="imgs/pep.jpg" alt="player">
+                      <span class="active-players__nick">nick</span>
+                    </div>
+                    <div class="active-players__betamount">
+                      <span class="active-players__betamount__amount-red">${counterRed.textContent}</span>
+                      <span class="active-players__betamount__color-red"></span>
+                    </div> 
+        `;
+    RedActivePlayers.insertAdjacentHTML("afterbegin", html);
+  }
+
+  //   else if( +balance.textContent < inputBetAmount.value) {
+  //     need to show some err in the chat, thats how on poligon// plz login or your balance is lover then bet go deposit
+  //   }
+});
+// console.log(clickCounterActivePlayers);
+
+bntZero.addEventListener("click", function () {
+  if (
+    inputBetAmount.value > 0 &&
+    +balance.textContent >= inputBetAmount.value
+  ) {
+    counterZero.textContent = +counterZero.textContent + +inputBetAmount.value;
+    totalBetAmountZero.textContent =
+      +totalBetAmountZero.textContent + +inputBetAmount.value;
+    // totalPlayersRed.textContent = +totalPlayersRed.textContent + 1; need to think how to do;
+  }
+});
+
+bntAllBlack.addEventListener("click", function () {
+  if (
+    inputBetAmount.value > 0 &&
+    +balance.textContent >= inputBetAmount.value
+  ) {
+    counterBlack.textContent =
+      +counterBlack.textContent + +inputBetAmount.value;
+    totalBetAmountBlack.textContent =
+      +totalBetAmountBlack.textContent + +inputBetAmount.value;
+    // totalPlayersRed.textContent = +totalPlayersRed.textContent + 1; need to think how to do;
+  }
+});
+
+//////////////////////////roullete
+function rand(min, max) {
+  //   return Math.floor(Math.random() * (max - min)) + min;
+  return Math.ceil(Math.random() * (max - min)) + min;
+}
+const arrWinCircle = [3, 5, 9, 11, 0, 2, 4, 0, 13, 7];
+function play() {
+  //add sound if you want
+  //var audio = new Audio('spin.mp3');
+  //audio.play();
+
+  const arrWinNumbs = [
+    1, 14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0,
+    11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1,
+    14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0,
+    11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1,
+    14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0,
+    11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1,
+    14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0,
+    11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1,
+    14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8,
+  ];
+
+  //   const offset = rand(75, 90 * 140); //90*140=12600
+  const offset = rand(3600, 10800);
+  document.getElementById("fillMeUpDaddy").style.left = -offset + "px";
+
+  //win number logic/ made this way because we start for number 1, but pointer line is locaited at number 6, so I add 450, which is the px from the beggining(left) and we get write shown win number;
+  //   const winnerNumber = offset + 560;
+  const winnerNumber = offset + 453;
+
+  const winNumber = arrWinNumbs[`${Math.floor(winnerNumber / 80)}`];
+  console.log(winNumber); //win number from array;
+
+  /////////function to change last 10 win numbers as interface;
+
+  const addWinNumber = function () {
+    arrWinCircle.pop();
+    arrWinCircle.unshift(`${winNumber}`);
+
+    const style2 = function (el) {
+      if (arrWinCircle[el] < 8 && arrWinCircle[el] > 0) {
+        return 2;
+      } else if (arrWinCircle[el] > 7) {
+        return 3;
+      } else arrWinCircle[el] === 0;
+      return 1;
+    };
+    winNumbersArr.innerHTML = "";
+    const winNumberArrHTML = `
+    <div
+    class="main-roullet__win-numbers__numbers__el main-roullet__win-numbers__numbers__el__${style2(
+      0
+    )}"
+  >
+    ${arrWinCircle[0]}
+  </div>
+  <div
+    class="main-roullet__win-numbers__numbers__el main-roullet__win-numbers__numbers__el__${style2(
+      1
+    )}"
+  >
+  ${arrWinCircle[1]}
+  </div>
+  <div
+    class="main-roullet__win-numbers__numbers__el main-roullet__win-numbers__numbers__el__${style2(
+      2
+    )}"
+  >
+  ${arrWinCircle[2]}
+  </div>
+  <div
+    class="main-roullet__win-numbers__numbers__el main-roullet__win-numbers__numbers__el__${style2(
+      3
+    )}"
+  >
+  ${arrWinCircle[3]}
+  </div>
+  <div
+    class="main-roullet__win-numbers__numbers__el main-roullet__win-numbers__numbers__el__${style2(
+      4
+    )}"
+  >
+  ${arrWinCircle[4]}
+  </div>
+  <div
+    class="main-roullet__win-numbers__numbers__el main-roullet__win-numbers__numbers__el__${style2(
+      5
+    )}"
+  >
+  ${arrWinCircle[5]}
+  </div>
+  <div
+    class="main-roullet__win-numbers__numbers__el main-roullet__win-numbers__numbers__el__${style2(
+      6
+    )}"
+  >
+  ${arrWinCircle[6]}
+  </div>
+  <div
+    class="main-roullet__win-numbers__numbers__el main-roullet__win-numbers__numbers__el__${style2(
+      7
+    )}"
+  >
+  ${arrWinCircle[7]}
+  </div>
+  <div
+    class="main-roullet__win-numbers__numbers__el main-roullet__win-numbers__numbers__el__${style2(
+      8
+    )}"
+  >
+  ${arrWinCircle[8]}
+  </div>
+  <div
+    class="main-roullet__win-numbers__numbers__el main-roullet__win-numbers__numbers__el__${style2(
+      9
+    )}"
+  >
+  ${arrWinCircle[9]}
+  </div>
+    `;
+    winNumbersArr.insertAdjacentHTML("afterbegin", winNumberArrHTML);
+  };
+  addWinNumber();
+  /////////////
+
+  //   console.log(arrWinNumbs[3]);
+  //   console.log(offset);
+  //   console.log(arrWinNumbs.length);
+  //   console.log(winnerNumber);
+  //   console.log(winnerNumber / 80);
+
+  //   let chosenPlayer = document.createElement("div");
+  //   chosenPlayer.style.backgroundColor = bets[order[parseInt(offset / 140)]][1];
+  //   chosenPlayer.innerHTML = bets[order[parseInt(offset / 140)]][0];
+  //   setTimeout(function () {
+  //     // document.getElementById("winners").appendChild(chosenPlayer);
+  //     document.getElementById("fillMeUpDaddy").style.transitionDuration = "0s";
+  //     document.getElementById("fillMeUpDaddy").style.left = "0px";
+  //     setTimeout(function () {
+  //       document.getElementById("fillMeUpDaddy").style.transitionDuration = "5s";
+  //     }, 50);
+  //   }, 5500);
+}
+
+btn10.addEventListener("click", function (e) {
+  //   init();
+  play();
+});
+
+//////////////Timer
 const startLogOutTimer = function () {
   const tick = function () {
-    // const min = String(Math.trunc(time / 60)).padStart(2, 0);
     const sec = String(time).padStart(2, 0);
     labelProgresBar.classList.remove("opacity");
     // In each call, print the remaining time to UI
@@ -87,124 +413,3 @@ const startLogOutTimer = function () {
 // startLogOutTimer();
 
 // when i will add spinning roullet => need to add startLogOutTimer(); when the spin is over;
-
-// background pisition
-
-//bets buttons for increasing and decreasing bet
-btnClear.addEventListener("click", function () {
-  inputBetAmount.value = 0;
-});
-
-btn10.addEventListener("click", function () {
-  inputBetAmount.value = +inputBetAmount.value + 10;
-});
-
-btn100.addEventListener("click", function () {
-  inputBetAmount.value = +inputBetAmount.value + 100;
-});
-
-btn1000.addEventListener("click", function () {
-  inputBetAmount.value = +inputBetAmount.value + 1000;
-});
-
-btn12.addEventListener("click", function () {
-  inputBetAmount.value = +inputBetAmount.value / 2;
-});
-
-btnX2.addEventListener("click", function () {
-  inputBetAmount.value = +inputBetAmount.value * 2;
-});
-
-btnMAX.addEventListener("click", function () {
-  inputBetAmount.value = balance.textContent;
-});
-
-balanceReload.addEventListener("click", function () {
-  balanceReload.classList.add("spin");
-});
-
-//roullete
-// var amountOfBoxes = 100;
-
-// var order = [];
-
-// function init() {
-//   let roulette = document.getElementById("fillMeUpDaddy");
-//   roulette.innerHTML = "";
-//   let tempBets = [
-//     ["player1", "red"],
-//     ["player2", "yellowgreen"],
-//   ];
-//   for (var i = 0; i < amountOfBoxes; i++) {
-//     var randomPlayer = rand(0, tempBets.length);
-//     var node = document.createElement("div");
-//     var h3 = document.createElement("h3");
-//     h3.innerHTML = tempBets[randomPlayer][0];
-//     h3.style.backgroundColor = tempBets[randomPlayer][1];
-//     node.appendChild(h3);
-//     roulette.appendChild(node);
-//   }
-// }
-
-function rand(min, max) {
-  //   return Math.floor(Math.random() * (max - min)) + min;
-  return Math.ceil(Math.random() * (max - min)) + min;
-}
-
-function play() {
-  //add sound if you want
-  //var audio = new Audio('spin.mp3');
-  //audio.play();
-
-  const arrWinNumbs = [
-    1, 14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0,
-    11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1,
-    14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0,
-    11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1,
-    14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0,
-    11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1,
-    14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0,
-    11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8, 1,
-    14, 2, 13, 3, 12, 4, 0, 11, 5, 10, 6, 9, 7, 8,
-  ];
-
-  //   const offset = rand(75, 90 * 140); //90*140=12600
-  const offset = rand(3600, 10800);
-
-  document.getElementById("fillMeUpDaddy").style.left = -offset + "px";
-
-  //win number logic/ made this way because we start for number 1, but pointer line is locaited at number 6, so I add 450, which is the px from the beggining(left) and we get write shown win number;
-  //   const winnerNumber = offset + 560;
-  const winnerNumber = offset + 453;
-
-  const winNumber = arrWinNumbs[`${Math.floor(winnerNumber / 80)}`];
-  console.log(winNumber); //win number from array;
-
-  //   console.log(arrWinNumbs[3]);
-  //   console.log(offset);
-  //   console.log(arrWinNumbs.length);
-  //   console.log(winnerNumber);
-  //   console.log(winnerNumber / 80);
-
-  //   let chosenPlayer = document.createElement("div");
-  //   chosenPlayer.style.backgroundColor = bets[order[parseInt(offset / 140)]][1];
-  //   chosenPlayer.innerHTML = bets[order[parseInt(offset / 140)]][0];
-  //   setTimeout(function () {
-  //     // document.getElementById("winners").appendChild(chosenPlayer);
-  //     document.getElementById("fillMeUpDaddy").style.transitionDuration = "0s";
-  //     document.getElementById("fillMeUpDaddy").style.left = "0px";
-  //     setTimeout(function () {
-  //       document.getElementById("fillMeUpDaddy").style.transitionDuration = "5s";
-  //     }, 50);
-  //   }, 5500);
-}
-
-btn10.addEventListener("click", function (e) {
-  //   init();
-  play();
-});
-
-// 195 cubes
-// 1 cube 80px
-// 195*80= 15600
-// 15600/195 = 80 px per one cube;
