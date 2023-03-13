29846d9bc46cf6f5f9db73f202435c610744a959
Andy-Kushtan <kushtanandriy17@gmail.com>
2023-03-13 19:15:32 +0200

    google login

--- script.js
+++ script.js
@@ -432,3 +432,21 @@ const startProgresBarTimer = function () {
 
 // startProgresBarTimer();
 // setInterval(startProgresBarTimer, 30000);
+
+///////////////google sign in
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  // $("#id of html element (he use name)").text(profile.getName());
+  $("image").attr("src", profile.getImageUrl());
+  $(".google__data").css("display", "flex");
+  $(".g-signin2").css("display", "none");
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log("User signed out.");
+    $(".g-signin2").css("display", "flex");
+    $(".google__data").css("display", "none");
+  });
+}
