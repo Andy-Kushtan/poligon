ca6bb6f2a5642485e59d1f51c8fb074240ee20de
Andy-Kushtan <kushtanandriy17@gmail.com>
2023-03-14 23:29:20 +0200

    test2

--- script.js
+++ script.js
@@ -450,13 +450,20 @@ const startProgresBarTimer = function () {
 //   `;
 //   googleInfo.insertAdjacentHTML("afterbegin", googleHTML);
 // }
-
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log("User signed out.");
-    // $(".g-signin2").css("display", "flex");
-    // $(".google__data").css("display", "none");
-    googleInfo.classList.add("display");
-  });
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log("Name: " + profile.getName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
+
+// function signOut() {
+//   var auth2 = gapi.auth2.getAuthInstance();
+//   auth2.signOut().then(function () {
+//     console.log("User signed out.");
+//     // $(".g-signin2").css("display", "flex");
+//     // $(".google__data").css("display", "none");
+//     googleInfo.classList.add("display");
+//   });
+// }
